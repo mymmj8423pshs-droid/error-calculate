aa539f0f5b93b93791af90936cefdc95501064a5
mymmj8423pshs-droid <mymmj8423pshs@gmail.com>
2026-06-23 10:45:52 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ErrorCalculator from "./ErrorCalculator.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorCalculator />
+  </React.StrictMode>
+);
